12bc0a6ae9812b985f70b388c2c4d11b71ae9b4a
naulyvirya <naulyhp16@gmail.com>
2019-11-01 08:49:20 +0700

    First CRUD

---
+++ database.php
@@ -0,0 +1,27 @@
+<?php
+class Database
+{
+    public $host = "localhost", $user = "root", $pass = 123, $db = "xirpl2";
+    public $koneksi;
+
+    public function __construct()
+    {
+        $this->koneksi = mysqli_connect(
+            $this->host,
+            $this->user,
+            $this->pass,
+            $this->db
+        );
+        if ($this->koneksi) {
+            # echo "Berhasil";
+        } else {
+            echo "Koneksi Database Gagal";
+        }
+    }
+}
+// Data tabel siswa
+include 'siswa.php';
+
+// Data tabel DB
+$db = new Database();
+?>
---
+++ siswa.php
@@ -0,0 +1,53 @@
+<?php
+class siswa extends Database
+{
+    // Menampilkan Semua Data
+    public function index()
+    {
+        $datasiswa = mysqli_query($this->koneksi, "select * from siswa");
+        // var_dump($datasiswa);
+        return $datasiswa;
+    }
+
+    // Menambah Data
+    public function create($nis, $nama, $alamat)
+    {
+        mysqli_query(
+            $this->koneksi,
+            "insert into siswa values(null,'$nis','$nama','$alamat'"
+        );
+    }
+    // Menampilkan Data Berdasarkan ID
+    public function show($id)
+    {
+        $datasiswa = mysqli_query($this->koneksi, "select * from siswa where id='$id'");
+        return $datasiswa;
+    }
+
+    // Menampilkan Data Berdasarkan id
+    public function edit($id)
+    {
+        $datasiswa = mysqli_query(
+            $this->koneksi,
+            "select * from siswa where id='$id'"
+        );
+        return $datasiswa;
+    }
+
+    // Mengupdate data Berdasarkan id
+    public function update($id, $nis, $nama, $alamat)
+    {
+        mysqli_query(
+            $this->koneksi,
+            "update siswa set nis = '$nis', nama = '$nama',
+        alamat = '$alamat' where id='$id'"
+        );
+    }
+
+    // menghapus data berdasarkan id
+    public function delete($id)
+    {
+        mysqli_query($this->koneksi, "delete from siswa where id='$id'");
+    }
+}
+?>
